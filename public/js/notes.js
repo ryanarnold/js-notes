2d45a588638c3d156d7f68dd5528ec61ce3473e5
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-02 17:51:44 +0800

    Add JS code to save notes in local storage

---
+++ public/js/notes.js
@@ -0,0 +1,47 @@
+
+/********************* Elements *********************/
+
+const elemNoteAdd = document.getElementById('noteAdd');
+const buttonAdd = document.getElementById('btnAdd');
+
+/********************* Functions *********************/
+
+function createNote() {
+  let text = elemNoteAdd.value.trimRight();
+
+  // Exit if note is empty
+  if (text === '') {
+    alert('Empty note!');
+    return;
+  }
+
+  let note = {
+    text
+  };
+
+  // Try to load 'notes' list in local storage
+  let notes = JSON.parse(localStorage.getItem('notes'));
+  
+  // If 'notes' list already exists in local storage append to it, else create new
+  if (notes) {
+    notes.push(note);
+  } else {
+    notes = [note];
+  }
+  localStorage.setItem('notes', JSON.stringify(notes));
+
+  // Clear textarea
+  elemNoteAdd.value = '';
+}
+
+/******************* Event Binding *******************/
+
+// Trigger note saving on CTRL + ENTER shortcut
+elemNoteAdd.addEventListener('keydown', (event) => {
+  if (event.ctrlKey && event.keyCode === 13) {
+    createNote();
+  }
+});
+
+// Trigger note saving on 'Add' button click
+buttonAdd.addEventListener('click', (event) => createNote);
