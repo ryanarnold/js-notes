38c37ae0790f858fa96a6c77fa6a02c0bdd7bba0
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-02 19:00:37 +0800

    Add functionality for clear button

--- public/js/notes.js
+++ public/js/notes.js
@@ -25,6 +25,7 @@ class Note {
 
 const elemNoteAdd = document.getElementById('noteAdd');
 const buttonAdd = document.getElementById('btnAdd');
+const buttonClear = document.getElementById('btnClear');
 
 /********************* Functions *********************/
 
@@ -96,3 +97,9 @@ buttonAdd.addEventListener('click', (event) => {
 
 // Display notes
 document.body.onload = loadSavedNotes;
+
+// Clear text area
+buttonClear.addEventListener('click', (event) => {
+  elemNoteAdd.value = '';
+  buttonClear.blur();
+});
