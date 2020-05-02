5c86487d2accaeacff5c51740ceda91c41205add
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-02 17:27:26 +0800

    Move textare autoresizing code to its own script

---
+++ public/js/textarea.js
@@ -0,0 +1,36 @@
+
+/* Text area resize utility functions */
+
+var observe;
+if (window.attachEvent) {
+  observe = function (element, event, handler) {
+      element.attachEvent('on'+event, handler);
+  };
+}
+else {
+  observe = function (element, event, handler) {
+      element.addEventListener(event, handler, false);
+  };
+}
+function textAreaInit () {
+  var text = document.getElementById('noteAdd');
+  function resize () {
+      text.style.height = 'auto';
+      text.style.height = text.scrollHeight+'px';
+  }
+  /* 0-timeout to get the already changed text */
+  function delayedResize () {
+      window.setTimeout(resize, 0);
+  }
+  observe(text, 'change',  resize);
+  observe(text, 'cut',     delayedResize);
+  observe(text, 'paste',   delayedResize);
+  observe(text, 'drop',    delayedResize);
+  observe(text, 'keydown', delayedResize);
+
+  text.focus();
+  text.select();
+  resize();
+}
+
+textAreaInit();
