9f1e6e6591886577a4b427e9c69029808c4236ae
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-02 16:29:43 +0800

    Serve index.html in / URL

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.use(express.static('public'));
 
