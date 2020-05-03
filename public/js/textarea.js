4b6ed05fe988fffe700964c157d0a8ca478320bf
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-03 19:42:10 +0800

    Apply textarea autoresizing on all textarea elements

--- public/js/textarea.js
+++ public/js/textarea.js
@@ -13,24 +13,28 @@ else {
   };
 }
 function textAreaInit () {
-  var text = document.getElementById('noteAdd');
-  function resize () {
-      text.style.height = 'auto';
-      text.style.height = text.scrollHeight+'px';
-  }
-  /* 0-timeout to get the already changed text */
-  function delayedResize () {
-      window.setTimeout(resize, 0);
-  }
-  observe(text, 'change',  resize);
-  observe(text, 'cut',     delayedResize);
-  observe(text, 'paste',   delayedResize);
-  observe(text, 'drop',    delayedResize);
-  observe(text, 'keydown', delayedResize);
+  var textarea = document.querySelectorAll('textarea');
 
-  text.focus();
-  text.select();
-  resize();
+  textarea.forEach(text => {
+    function resize () {
+        text.style.height = 'auto';
+        text.style.height = text.scrollHeight+'px';
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    text.focus();
+    text.select();
+    resize();
+  });
+  
 }
 
 textAreaInit();
