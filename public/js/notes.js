6533bb2d9da941aa9bbd5792d36e3ae3b69d064b
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-05-10 12:48:32 +0800

    Install eslint

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,7 +1,14 @@
 
-/********************* Classes *********************/
+/** Classes *******************************************************************/
 
+/**
+ * Represents Note object
+ */
 class Note {
+  /**
+   * Constructor
+   * @param {string} text Note's text
+   */
   constructor(text) {
     this.id = Math.floor(Math.random() * 99999) + 10000;
     this.text = text;
@@ -21,26 +28,34 @@ class Note {
   }
 }
 
-/********************* Elements *********************/
+/** Elements ******************************************************************/
 
 const elemNoteAdd = document.getElementById('noteAdd');
 const buttonAdd = document.getElementById('btnAdd');
 const buttonClear = document.getElementById('btnClear');
 let elemNotes = document.querySelectorAll('.note');
-let elemEmptyNotes = document.getElementById('emptyNotes');
+const elemEmptyNotes = document.getElementById('emptyNotes');
 
-/********************* Functions *********************/
+/** Functions *****************************************************************/
 
-
+/**
+ * Deletes a note from the local storate
+ * @param {int} noteIdToDelete Note to delete
+ */
 function deleteNote(noteIdToDelete) {
   let notes = JSON.parse(localStorage.getItem('notes'));
-  notes = notes.filter(note => note.id != noteIdToDelete);
+  notes = notes.filter((note) => note.id != noteIdToDelete);
   localStorage.setItem('notes', JSON.stringify(notes));
 }
 
+/**
+ * Update an existing note from the local storage
+ * @param {int} noteId Note to update
+ * @param {string} noteText New text of note
+ */
 function saveNote(noteId, noteText) {
   let notes = JSON.parse(localStorage.getItem('notes'));
-  notes = notes.map(note => {
+  notes = notes.map((note) => {
     if (note.id === noteId) {
       note.text = noteText;
       return note;
@@ -50,39 +65,42 @@ function saveNote(noteId, noteText) {
   localStorage.setItem('notes', JSON.stringify(notes));
 }
 
+/**
+ * Event handler for editing a note
+ * @param {Element} elem Element
+ */
 function editNote(elem) {
-  let noteP = elem.querySelector('p');
-  
-  let elemEditTextArea = document.createElement('textarea');
+  const noteP = elem.querySelector('p');
+
+  const elemEditTextArea = document.createElement('textarea');
   elemEditTextArea.className = 'text-edit';
   elemEditTextArea.innerHTML = noteP.innerHTML.replace(/<br>/gi, '\n');
   elemEditTextArea.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.keyCode === 13) {
       const parent = event.target.closest('.col-main');
-      let noteText = parent.querySelector('textarea').value;
-      let noteId = parent.noteId
+      const noteText = parent.querySelector('textarea').value;
+      const noteId = parent.noteId;
       saveNote(noteId, noteText);
       loadSavedNotes();
     } else if (event.keyCode === 27) {
       loadSavedNotes();
     }
-    
   });
   elem.append(elemEditTextArea);
 
-  let btnDiscard = document.createElement('button');
+  const btnDiscard = document.createElement('button');
   btnDiscard.className = 'btn btn-edit btn-discard';
-  btnDiscard.innerHTML = 'Discard'
+  btnDiscard.innerHTML = 'Discard';
   btnDiscard.addEventListener('click', loadSavedNotes);
   elem.append(btnDiscard);
 
-  let btnSave = document.createElement('button');
+  const btnSave = document.createElement('button');
   btnSave.className = 'btn btn-edit btn-save';
-  btnSave.innerHTML = 'Save'
+  btnSave.innerHTML = 'Save';
   btnSave.addEventListener('click', () => {
     const parent = event.target.closest('.col-main');
-    let noteText = parent.querySelector('textarea').value;
-    let noteId = parent.noteId
+    const noteText = parent.querySelector('textarea').value;
+    const noteId = parent.noteId;
     saveNote(noteId, noteText);
     loadSavedNotes();
   });
@@ -95,8 +113,11 @@ function editNote(elem) {
   textAreaInit();
 }
 
+/**
+ * Bind note events
+ */
 function bindNoteEvents() {
-  elemNotes.forEach(elem => {
+  elemNotes.forEach((elem) => {
     elem.addEventListener('mouseover', (event) => {
       if (elem.editing === 'false') {
         elem.querySelector('.btn-delete').hidden = false;
@@ -109,20 +130,23 @@ function bindNoteEvents() {
 
     elem.addEventListener('click', () => {
       editNote(elem);
-    }, { once: true });
+    }, {once: true});
 
     const buttonDelete = elem.querySelector('.btn-delete');
-    
+
     buttonDelete.addEventListener('click', (event) => {
       event.preventDefault();
       deleteNote(elem.noteId);
       loadSavedNotes();
     });
-  });  
+  });
 }
 
+/**
+ * Event handler for creating a note
+ */
 function createNote() {
-  let text = elemNoteAdd.value.trimRight();
+  const text = elemNoteAdd.value.trimRight();
 
   // Exit if note is empty
   if (!text) {
@@ -130,12 +154,12 @@ function createNote() {
     return;
   }
 
-  let note = new Note(text);
+  const note = new Note(text);
 
   // Try to load 'notes' list in local storage
   let notes = JSON.parse(localStorage.getItem('notes'));
-  
-  // If 'notes' list already exists in local storage append to it, else create new
+
+  // If 'notes' list already exists in local storage append , else create new
   if (notes) {
     notes.push(note);
   } else {
@@ -150,25 +174,31 @@ function createNote() {
   loadSavedNotes();
 }
 
-
+/**
+ * Refresh notes feed
+ */
 function loadSavedNotes() {
   const notes = JSON.parse(localStorage.getItem('notes'));
 
-  let elemTop = document.querySelector('.subheading');
-  
+  const elemTop = document.querySelector('.subheading');
+
   // Clear notes list display
-  document.querySelectorAll('.note').forEach(e => e.remove());
+  document.querySelectorAll('.note').forEach((e) => e.remove());
 
   if (notes && notes.length != 0) {
     elemEmptyNotes.hidden = true;
     for (const note of notes) {
       note.textHTML = note.text.replace(/\r\n|\r|\n/gi, '<br>');
 
-      let elemNote = document.createElement('div');
+      const elemNote = document.createElement('div');
       elemNote.className = 'col-main note ' + note.color;
       elemNote.noteId = note.id;
       elemNote.editing = 'false';
-      elemNote.innerHTML = `<a href="" class="btn-delete" hidden><i class="gg-close"></i></a><p>${note.textHTML}</p>`
+      elemNote.innerHTML = `
+        <a href="" class="btn-delete" hidden>
+          <i class="gg-close"></i>
+        </a>
+        <p>${note.textHTML}</p>`;
 
       elemTop.after(elemNote);
     }
@@ -180,7 +210,7 @@ function loadSavedNotes() {
   bindNoteEvents();
 }
 
-/******************* Event Binding *******************/
+/** Event Binding *************************************************************/
 
 // Trigger note saving on CTRL + ENTER shortcut
 elemNoteAdd.addEventListener('keydown', (event) => {
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.use(express.static('public'));
