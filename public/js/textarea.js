d44b4b788c94f3dae160eb891ad935cff18ac259
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-06-23 09:22:45 +0800

    Fix eslint errors

--- public/js/textarea.js
+++ public/js/textarea.js
@@ -1,40 +1,38 @@
 
 /* Text area resize utility functions */
 
-var observe;
+let observe;
 if (window.attachEvent) {
-  observe = function (element, event, handler) {
-      element.attachEvent('on'+event, handler);
+  observe = function(element, event, handler) {
+    element.attachEvent('on'+event, handler);
+  };
+} else {
+  observe = function(element, event, handler) {
+    element.addEventListener(event, handler, false);
   };
 }
-else {
-  observe = function (element, event, handler) {
-      element.addEventListener(event, handler, false);
-  };
-}
-function textAreaInit () {
-  var textarea = document.querySelectorAll('textarea');
+function textAreaInit() {
+  const textarea = document.querySelectorAll('textarea');
 
-  textarea.forEach(text => {
-    function resize () {
-        text.style.height = 'auto';
-        text.style.height = text.scrollHeight+'px';
+  textarea.forEach((text) => {
+    function resize() {
+      text.style.height = 'auto';
+      text.style.height = text.scrollHeight+'px';
     }
     /* 0-timeout to get the already changed text */
-    function delayedResize () {
-        window.setTimeout(resize, 0);
+    function delayedResize() {
+      window.setTimeout(resize, 0);
     }
-    observe(text, 'change',  resize);
-    observe(text, 'cut',     delayedResize);
-    observe(text, 'paste',   delayedResize);
-    observe(text, 'drop',    delayedResize);
+    observe(text, 'change', resize);
+    observe(text, 'cut', delayedResize);
+    observe(text, 'paste', delayedResize);
+    observe(text, 'drop', delayedResize);
     observe(text, 'keydown', delayedResize);
 
     text.focus();
     text.select();
     resize();
   });
-  
 }
 
 textAreaInit();
